3b9655ca4b1f0c196b1fcbac7656bbd101561d89
Loreto Dumitrescu <loretodalt@gmail.com>
2022-01-15 09:20:08 -0500

    0.1.1

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.1.0","target":"arcade","targetVersion":"1.7.24","repo":"loretod/things-that-go-bump"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.7.24/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.1.1","target":"arcade","targetVersion":"1.7.24","repo":"loretod/things-that-go-bump"}
 // total=1296825 new=7.39% cached=87.29% other=5.32%
 (function (ectx) {
 'use strict';
@@ -414,7 +414,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P131514(s) {
+function _main___P131957(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -835,13 +835,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P131514.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P131514.continuations = [  ]
+_main___P131957.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P131957.continuations = [  ]
 
-function _main___P131514_mk(s) {
+function _main___P131957_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P131514, depth: s.depth + 1,
+        parent: s, fn: _main___P131957, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42949,5 +42949,5 @@ const sprites_StaticObstacle__C115751_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["transparency16___117375","fish___117373","sea_weed___117372"])
 
-return _main___P131514
+return _main___P131957
 })
